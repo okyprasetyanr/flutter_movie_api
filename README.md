d2af73aa7b17f4fadc30792d831ed329b29832ae
bluecode_breeze_android <okyprasetya89@gmail.com>
2026-04-29 12:51:08 +0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutte_movie_api
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutte_movie_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutte_movie_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
